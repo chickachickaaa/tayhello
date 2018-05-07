2ec7350408f0603212d7ac91423b9b71b13d1a88
Keira <higgins.taylor.a@gmail.com>
2018-05-06 23:56:37 -0400

    Update date_simple.py

--- date_simple.py
+++ date_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#DONE SUCCESS!! 
 
 import sys
 from datetime import datetime, date, time, timedelta
@@ -9,41 +10,41 @@ def get_date_object(date=None):
     #optional_string_date = input
     try:
         if date:
-            date_obj = datetime.datetime.strptime(date, '%Y-%m-%d').date() 
-    except ValueError:
-        raise ValueError('please enter a proper format')
-    except TypeError:
-        raise TypeError('please enter a string argument')	
-        #do i use raise or exit?
-    else:
+            dateobj1 = datetime.strptime(date, '%Y-%m-%d').date()
+            #print(dateobj1)
+        else:
+            dateobj1 = datetime.today().date()
             #getting a syntax error on else when I indent..
-        date_obj = date.today()
-        #AttributeError: 'NoneType' object has no attribute 'today'
-        print(date_obj)
-        print(type(date_obj))
-    return(date_obj)
+    except ValueError:
+        raise ValueError('please enter the date in format: year month day separated by a hyphen')
+    except TypeError:
+        raise TypeError('please enter a string')   
+         #do i use raise or exit?
+
+    #print(type(dateobj1))
+    return(dateobj1)
 
 def get_date_string(date_object=None): 
     """ takes an optional date object and returns a formatted string """
     try:
         if date_object:
-            #format string
-            date_str = datetime.datetime.strptime(date_object, '%m/%d/%y')
+            date_str = datetime.strftime(date_object, '%m/%d/%y')
+            #print(date_str)
+        else:
+            #getting a syntax error on else when I indent..
+            date_str = str(date.today())
     except TypeError:
         raise TypeError('please enter an date object')    
-    else:
-        #getting a syntax error on else when I indent..
-        date_str = str(date.today())
-        # print(date_str)
-        # print(type(date_str))
+        
+    #print(date_str)
     return(date_str)
 
 
 if __name__ == '__main__': 
     get_date_object()
-    get_date_string() #ValueError: unconverted data remains: 18 
+    #date='1776-7-4'
+    #date='2018-2-26'
+    get_date_string() 
+    #date_object=date(1941, 2, 7)
 
 #in this case, how should i structure so i have just main() called after this gateway?
-
-
-
