06cd63782b09c4289845ceac64e4e183a9447398
Keira <higgins.taylor.a@gmail.com>
2018-05-06 23:57:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup( name='tayhello',
-       version='0.1',
+       version='0.2',
        description='This module makes datetime module more simple. ',
-       url='https://github.com/chickachickaaa/tayhello.git',  # usually a github URL after commit
+       url='https://github.com/chickachickaaa/tayhello.git',  
        author='Taylor Higgins',
        author_email='thiggins4890@gmail.com',
        license='MIT',
        packages=['tayhello'],
        install_requires=[ ],
-       test_suite='pytest'
-       setup_requires=['pytest-runner']
-       tests_require=['pytest']
+       test_suite='pytest',
+       setup_requires=['pytest-runner'],
+       tests_require=['pytest'],
        zip_safe=False )
